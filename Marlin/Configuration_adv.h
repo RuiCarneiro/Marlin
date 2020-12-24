ea3abfd2ab5d63afc6ab26328c0303623b9ff464
Rui Carneiro <rui.nelson@hotmail.com>
2020-12-24 04:04:35 +0000

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2339,12 +2339,13 @@
    * Override for each driver with <driver>_INTERPOLATE settings below
    */
 #define INTERPOLATE true
+#define RAMBO_R_SENSE 0.22
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 600            // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT 500            // (mA) RMS current. Multiply by 1.414 for peak current.
 #define X_CURRENT_HOME X_CURRENT // (mA) RMS current for sensorless homing
 #define X_MICROSTEPS 16          // 0..256
-#define X_RSENSE 0.91
+#define X_RSENSE RAMBO_R_SENSE
 #define X_CHAIN_POS -1 // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
 //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
 #endif
@@ -2359,10 +2360,10 @@
 #endif
 
 #if AXIS_IS_TMC(Y)
-#define Y_CURRENT 600
+#define Y_CURRENT 500
 #define Y_CURRENT_HOME Y_CURRENT
 #define Y_MICROSTEPS 16
-#define Y_RSENSE 0.091
+#define Y_RSENSE RAMBO_R_SENSE
 #define Y_CHAIN_POS -1
 //#define Y_INTERPOLATE  true
 #endif
@@ -2377,10 +2378,10 @@
 #endif
 
 #if AXIS_IS_TMC(Z)
-#define Z_CURRENT 600
+#define Z_CURRENT 500
 #define Z_CURRENT_HOME Z_CURRENT
 #define Z_MICROSTEPS 16
-#define Z_RSENSE 0.091
+#define Z_RSENSE RAMBO_R_SENSE
 #define Z_CHAIN_POS -1
 //#define Z_INTERPOLATE  true
 #endif
@@ -2413,9 +2414,9 @@
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 800
+#define E0_CURRENT 600
 #define E0_MICROSTEPS 16
-#define E0_RSENSE 0.091
+#define E0_RSENSE RAMBO_R_SENSE
 #define E0_CHAIN_POS -1
 //#define E0_INTERPOLATE true
 #endif
@@ -2659,9 +2660,9 @@
 
 #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
 // TMC2209: 0...255. TMC2130: -64...63
-#define X_STALL_SENSITIVITY 8
+#define X_STALL_SENSITIVITY 5
 #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-#define Y_STALL_SENSITIVITY 1
+#define Y_STALL_SENSITIVITY 5
 #define Y2_STALL_SENSITIVITY Y_STALL_SENSITIVITY
 //#define Z_STALL_SENSITIVITY  8
 //#define Z2_STALL_SENSITIVITY Z_STALL_SENSITIVITY
